42efe9aae7342dddca535726294571dbf0f95a09
Bella Nicholson <nicholsonbella@gmail.com>
2024-11-18 22:28:16 +0100

    fix: don't use nightly pypi package

--- ios/MLCChat/setup.sh
+++ ios/MLCChat/setup.sh
@@ -5,11 +5,7 @@
 echo "Installing rust and cargo..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo "Setting up python virtual env..."
-poetry init
-poetry shell
-
 echo "Installing MLC LLM lib..."
-python -m pip install --pre -U -f https://mlc.ai/wheels mlc-llm-nightly-cpu mlc-ai-nightly-cpu
+python -m pip install --pre -U -f https://mlc.ai/wheels mlc-llm-cpu mlc-ai-cpu
 # Validate installation
 python -c "import mlc_llm; print(mlc_llm)"
