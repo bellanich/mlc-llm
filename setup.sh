de07e0e53acce10927208ca386f7ee9e53e71482
Bella Nicholson <nicholsonbella@gmail.com>
2024-11-24 13:11:18 +0100

    feat: copy custom model defns to compiled MLC lib

--- setup.sh
+++ setup.sh
@@ -5,7 +5,12 @@
 echo "Installing rust and cargo..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# Install STABLE version of MLC LLM lib
 echo "Installing MLC LLM lib..."
 python -m pip install --pre -U -f https://mlc.ai/wheels mlc-llm-cpu mlc-ai-cpu
+# Add in custom model definitions
+cp ios/MLCChat/models/siglip_vision.py .venv/lib/python3.10/site-packages/mlc_llm/model/vision/siglip_vision.py
+cp -r ios/MLCChat/models/llava_onevision .venv/lib/python3.10/site-packages/mlc_llm/model/llava_onevision
+
 # Validate installation
 python -c "import mlc_llm; print(mlc_llm)"
