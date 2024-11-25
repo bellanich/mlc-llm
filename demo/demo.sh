b9f4999c38a5304f9fbeb1b0592d645da0e930a5
Bella Nicholson <nicholsonbella@gmail.com>
2024-11-25 23:23:07 +0100

    feat: preview img as demo step

---
+++ demo/demo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+# Image filepath
+catimg "/Users/bellanicholson/Projects/mlc-llm/src/images/cat.jpg"
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.describe_image import describe_image
 
-# TODO: detect if img
+
 def simple_chatbot():
     print("Hello! I'm a multi-modal chatbot. Type something and I'll respond. Type 'exit' to leave the chat.")
     while True:
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     # (1) Write simple unit tests for describe_image() + helper funcs
     # (2) Separate image filepath from rest of user input
     # (3) Feed image_filepath -> describe_image() --> image_description --> gemma
+    # (4) Center + crop image rather than resizing it
+    # (5) Make a simple demo
 
     # How to generate description of image from image_filepath
     image_path = "src/images/cat.jpg"
